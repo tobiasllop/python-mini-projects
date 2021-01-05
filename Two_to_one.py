9f62348fb804ef446c9877471fac83891e80c17a
Tobias Llop <65455969+tobiasllop@users.noreply.github.com>
2021-01-05 13:00:28 -0300

    Update Two_to_one.py

--- Two_to_one.py
+++ Two_to_one.py
@@ -1,7 +1,10 @@
+s1 = str(input("Insert one random word: "))
+s2 = str(input("Insert another random word: "))
 def longest(s1, s2):
   string = (s1+s2).lower()
   string1 = "".join(set(string))
   string = sorted(string1)
   string = "".join(string)
-  print(string)
-longest("Tobias", "Llop")
+  print(f"Your two to one word is: {string}")
+
+longest(s1, s2)
