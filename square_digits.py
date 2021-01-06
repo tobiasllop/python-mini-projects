fced0cc1bd626b90c53c7e76d11950dd45d5dfa1
Tobias Llop <65455969+tobiasllop@users.noreply.github.com>
2021-01-06 11:32:21 -0300

    Update square_digits.py

--- square_digits.py
+++ square_digits.py
@@ -1,11 +1,14 @@
 import time
 def main():
+  #Square every digit of he number
   def square_digits(num):
     num = str(num)
     my_list = []
     for i in range(0, len(num)):
       square = int(num[i]) * int(num[i])
+      #Create a list with the result of the squared digits
       my_list.append(str(square))
+    #Join and print the list
     x = "" 
     print(x.join(my_list))  
 
@@ -13,6 +16,7 @@ def main():
   if len(num) < 2:
     print("Try again")
     time.sleep(1)
+    #Go back
     return main()
   else:
     square_digits(num)
